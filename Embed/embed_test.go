3d15b2fcfdc53164c7c571ff20e0f46287c567fb
Dams168 <adamrohman2003@gmail.com>
2026-02-16 23:27:01 +0700

    chore: Go Embed PZN

---
+++ Embed/embed_test.go
@@ -0,0 +1,55 @@
+package main_test
+
+import (
+	"embed"
+	_ "embed"
+	"fmt"
+	"io/fs"
+	"io/ioutil"
+	"testing"
+)
+
+//go:embed version.txt
+var version string
+
+func TestVersion(t *testing.T) {
+	fmt.Println(version)
+}
+
+//go:embed logo.png
+var logo []byte
+
+func TestLogo(t *testing.T) {
+	err := ioutil.WriteFile("logo_new.png", logo, fs.ModePerm)
+	if err != nil {
+		panic(err)
+	}
+}
+
+//go:embed files/a.txt
+//go:embed files/b.txt
+//go:embed files/c.txt
+var files embed.FS
+
+func TestFiles(t *testing.T) {
+	a, _ := files.ReadFile("files/a.txt")
+	fmt.Println(string(a))
+	b, _ := files.ReadFile("files/b.txt")
+	fmt.Println(string(b))
+	c, _ := files.ReadFile("files/c.txt")
+	fmt.Println(string(c))
+}
+
+//go:embed files/*.txt
+var allFiles embed.FS
+
+func TestAllFiles(t *testing.T) {
+	entries, _ := allFiles.ReadDir("files")
+	for _, entry := range entries {
+		if !entry.IsDir() {
+			fmt.Println(entry.Name())
+			file, _ := allFiles.ReadFile("files/" + entry.Name())
+			fmt.Println(string(file))
+		}
+	}
+}
